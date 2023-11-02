9c19d6e507bb788e21cbc329bd1db9c9a94dfe40
Yoshitaka motomura <stay.daybreak@gmail.com>
2023-11-03 01:43:53 +0900

    :docs: merge apiblueprint

--- documentation/.vuepress/config.js
+++ documentation/.vuepress/config.js
@@ -3,12 +3,15 @@ import { defaultTheme } from '@vuepress/theme-default'
 
 export default defineUserConfig({
   lang: 'ja-JP',
-  title: 'VuePress',
-  description: 'Just playing around',
+  title: 'AWS CDK App Template',
+  description: 'AWS CDK App Template',
   dest: './docs',
   base: '/aws-cdk-app-template/', // for github pages repository name
   theme: defaultTheme({
-    navbar: [{ text: 'Home', link: '/' }],
+    navbar: [
+      { text: 'Home', link: '/' },
+      { text: 'API', link: '/api/' },
+    ],
     repo: 'https://github.com/yoshitaka-motomura/aws-cdk-app-template',
     sidebar: [
       {
